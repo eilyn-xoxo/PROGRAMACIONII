925801eefa53c2eb89c3dc0a57e8b5e41eecf538
eilyn-xoxo <eilynfortunaperezramirez@gmail.com>
2023-08-13 21:57:16 -0400

    Add project files.

---
+++ ProyectoHotel/Controllers/MantenimientoController.cs
@@ -0,0 +1,110 @@
+﻿using ProyectoHotel.Logica;
+using ProyectoHotel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoHotel.Controllers
+{
+    public class MantenimientoController : Controller
+    {
+        // GET: Mantenimiento
+        public ActionResult Habitaciones()
+        {
+            if (Session["Usuario"] == null)
+                return RedirectToAction("Index", "Login");
+
+            return View();
+        }
+
+        // GET: Mantenimiento
+        public ActionResult Categoria()
+        {
+            if (Session["Usuario"] == null)
+                return RedirectToAction("Index", "Login");
+
+            return View();
+        }
+
+        // GET: Mantenimiento
+        public ActionResult Entrada()
+        {
+            if (Session["Usuario"] == null)
+                return RedirectToAction("Index", "Login");
+
+            return View();
+        }
+
+
+
+        [HttpGet]
+        public JsonResult ListarCategoria()
+        {
+            List<Categoria> oLista = new List<Categoria>();
+            oLista = CategoriaLogica.Instancia.Listar();
+            return Json(new { data = oLista }, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult GuardarCategoria(Categoria objeto)
+        {
+            bool respuesta = false;
+            respuesta = (objeto.IdCategoria == 0) ? CategoriaLogica.Instancia.Registrar(objeto) : CategoriaLogica.Instancia.Modificar(objeto);
+            return Json(new { resultado = respuesta }, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult EliminarCategoria(int id)
+        {
+            bool respuesta = false;
+            respuesta = CategoriaLogica.Instancia.Eliminar(id);
+            return Json(new { resultado = respuesta }, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpGet]
+        public JsonResult ListarEntrada()
+        {
+            List<Entrada> oLista = new List<Entrada>();
+            oLista = EntradaLogica.Instancia.Listar();
+            return Json(new { data = oLista }, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult GuardarEntrada(Entrada objeto)
+        {
+            bool respuesta = false;
+            respuesta = (objeto.IdEntrada == 0) ? EntradaLogica.Instancia.Registrar(objeto) : EntradaLogica.Instancia.Modificar(objeto);
+            return Json(new { resultado = respuesta }, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult EliminarEntrada(int id)
+        {
+            bool respuesta = false;
+            respuesta = EntradaLogica.Instancia.Eliminar(id);
+            return Json(new { resultado = respuesta }, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpGet]
+        public JsonResult ListarHabitacion()
+        {
+            List<Habitacion> oLista = new List<Habitacion>();
+            oLista = HabitacionLogica.Instancia.Listar();
+            return Json(new { data = oLista }, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult GuardarHabitacion(Habitacion objeto)
+        {
+            bool respuesta = false;
+            respuesta = (objeto.IdHabitacion == 0) ? HabitacionLogica.Instancia.Registrar(objeto) : HabitacionLogica.Instancia.Modificar(objeto);
+            return Json(new { resultado = respuesta }, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult EliminarHabitacion(int id)
+        {
+            bool respuesta = false;
+            respuesta = HabitacionLogica.Instancia.Eliminar(id);
+            return Json(new { resultado = respuesta }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ ProyectoHotel/Logica/PisoLogica.cs
@@ -0,0 +1,156 @@
+﻿using ProyectoHotel.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoHotel.Logica
+{
+    public class EntradaLogica
+    {
+        private static EntradaLogica instancia = null;
+
+        public EntradaLogica()
+        {
+
+        }
+
+        public static EntradaLogica Instancia
+        {
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new EntradaLogica();
+                }
+
+                return instancia;
+            }
+        }
+
+        public bool Registrar(Entrada oEntrada)
+        {
+            bool respuesta = true;
+            using (SqlConnection oConexion = new SqlConnection(Conexion.CN))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("sp_RegistrarEntrada", oConexion);
+                    cmd.Parameters.AddWithValue("Descripcion", oEntrada.Descripcion);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oConexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    respuesta = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+
+                }
+                catch (Exception ex)
+                {
+                    respuesta = false;
+                }
+            }
+            return respuesta;
+        }
+
+        public bool Modificar(Entrada oEntrada)
+        {
+            bool respuesta = true;
+            using (SqlConnection oConexion = new SqlConnection(Conexion.CN))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("sp_ModificarEntrada", oConexion);
+                    cmd.Parameters.AddWithValue("IdEntrada", oEntrada.IdEntrada);
+                    cmd.Parameters.AddWithValue("Descripcion", oEntrada.Descripcion);
+                    cmd.Parameters.AddWithValue("Estado", oEntrada.Estado);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oConexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    respuesta = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+
+                }
+                catch (Exception ex)
+                {
+                    respuesta = false;
+                }
+
+            }
+
+            return respuesta;
+
+        }
+
+
+        public List<Entrada> Listar()
+        {
+            List<Entrada> Lista = new List<Entrada>();
+            using (SqlConnection oConexion = new SqlConnection(Conexion.CN))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("select IdEntrada,Descripcion,Estado from Entrada", oConexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    oConexion.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            Lista.Add(new Entrada()
+                            {
+                                IdEntrada = Convert.ToInt32(dr["IdEntrada"]),
+                                Descripcion = dr["Descripcion"].ToString(),
+                                Estado = Convert.ToBoolean(dr["Estado"])
+                            });
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Lista = new List<Entrada>();
+                }
+            }
+            return Lista;
+        }
+
+        public bool Eliminar(int id)
+        {
+            bool respuesta = true;
+            using (SqlConnection oConexion = new SqlConnection(Conexion.CN))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("delete from Entrada where idEntrada = @id", oConexion);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.CommandType = CommandType.Text;
+
+                    oConexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    respuesta = true;
+
+                }
+                catch (Exception ex)
+                {
+                    respuesta = false;
+                }
+
+            }
+
+            return respuesta;
+
+        }
+    }
+}
